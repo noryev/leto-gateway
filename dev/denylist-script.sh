0391e1815b820a5a0489c4296d7609d49ff908fb
Logan Lentz <logan.lentz1@gmail.com>
2023-01-24 22:09:32 -0600

    denylist script for linux EC2 instance

---
+++ dev/denylist-script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# specify the path to the default IPFS storage directory
+IPFS_STORAGE_DIR="~/.ipfs"
+
+# specify the deny-list file containing the CIDs of the files to be removed
+DENY_LIST_FILE="deny-list.json"
+
+# read the deny-list file
+CIDS=$(jq -r '.[]' $DENY_LIST_FILE)
+
+# loop through the CIDs in the deny-list file
+for CID in $CIDS; do
+    # run the ipfs pin rm command to remove each CID
+    ipfs pin rm $CID
+done
