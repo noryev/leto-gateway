aabd0683a92cc91a049abb9dac4ea11aa89018cd
Logan Lentz <logan.lentz1@gmail.com>
2023-01-24 22:12:36 -0600

    acessability requred to run script

--- dev/denylist-script.sh
+++ dev/denylist-script.sh
@@ -4,7 +4,7 @@
 IPFS_STORAGE_DIR="~/.ipfs"
 
 # specify the deny-list file containing the CIDs of the files to be removed
-DENY_LIST_FILE="deny-list.json"
+DENY_LIST_FILE="/path/to/deny-list.json"
 
 # read the deny-list file
 CIDS=$(jq -r '.[]' $DENY_LIST_FILE)
@@ -14,3 +14,4 @@ for CID in $CIDS; do
     # run the ipfs pin rm command to remove each CID
     ipfs pin rm $CID
 done
+
